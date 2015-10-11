542db1e0adedbecef6363c31cf6f6855a50db164
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-10-10 23:03:25 -0400

    Exit button menu and handler.
    Press the exit button :)

---
+++ Assets/Scripts/menuButtonHandlers.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class menuButtonHandlers : MonoBehaviour {
+
+    public void NewGame()
+    {
+        Application.LoadLevel("Level1");
+    }
+
+    public void ExitGame()
+    {
+        quit = !quit;
+    }
+
+public bool quit  = false;
+
+    void OnGUI()
+    {
+        if (quit)
+        { 
+            GUI.Label(new Rect(638, 600, 200, 100), "Would You like to quit the game?");
+            if (GUI.Button(new Rect(628, 630, 100, 100), "Yes"))
+            {
+                Application.Quit();
+            }
+            if (GUI.Button(new Rect(738, 630, 100, 100), "No"))
+            {
+                quit = false;
+            }
+        }
+    }
+}
+
