3ce8b9a74de56250da812047cc0470ab85f4e1c4
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-10-10 21:44:54 -0400

    new script for exit button
    dialog and exit.

--- Assets/Scripts/menuButtonNew.cs
+++ Assets/Scripts/menuButtonNew.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEditor;
 
 public class menuButtonNew : MonoBehaviour {
 
     public void NewGame(){
         Application.LoadLevel("Level1");
+    }
+
+    public void ExitGame()
+    {
+        if(EditorUtility.DisplayDialog("Exit the Game?", "Do you really want to exit this game?", "Exit Game", "Cancel")){
+            Application.Quit();
+        }
     }
 
 }
